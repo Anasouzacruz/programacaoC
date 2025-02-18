477661bba9d3a84204e7202ca20ce896b597b376
programacaogithub <vaibrilhar@yahoo.com.br>
2025-02-18 20:48:48 -0300

    Primeira atualização

---
+++ .vscode/operadorlogico.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+    {
+        int a=10, b=-5;
+
+        if(a>0 && b>0) 
+        {
+            printf("Ambos os números são positivos\n");
+        } else {
+            printf("Ao menos um dos números é negativo\n");
+        }
+
+        return 0;
+
+}
